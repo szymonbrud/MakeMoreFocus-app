4d4988ac45f3d82afb0ff2e5b6aca487b7b3afac
szymonqqaz <szymonqqaz22@gmail.com>
2019-06-23 15:08:58 +0200

    add react-pose and some animation h1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MainTemplate from 'templates/MainTemplate';
 import styled from 'styled-components';
+import posed from 'react-pose';
+
+const AnimateH1 = posed.h1({
+  hiddene: {
+    opacity: 0,
+    x: '-100%',
+  },
+  visiblee: {
+    opacity: 1,
+    x: 0,
+  },
+});
 
 const StyledWrapper = styled.div`
   width: 100%;
@@ -12,16 +24,32 @@ const StyledWrapper = styled.div`
   justify-content: center;
 `;
 
-const H1 = styled.h1`
+const H1 = styled(AnimateH1)`
   margin: 0;
 `;
 
-const App = () => (
-  <MainTemplate>
-    <StyledWrapper>
-      <H1>hello world</H1>
-    </StyledWrapper>
-  </MainTemplate>
-);
+class App extends Component {
+  state = {
+    animate: true,
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState(prevState => ({ animate: !prevState.animate }));
+    }, 1000);
+  }
+
+  render() {
+    const { animate } = this.state;
+
+    return (
+      <MainTemplate>
+        <StyledWrapper>
+          <H1 pose={animate ? 'visiblee' : 'hiddene'}>hello world</H1>
+        </StyledWrapper>
+      </MainTemplate>
+    );
+  }
+}
 
 export default App;
